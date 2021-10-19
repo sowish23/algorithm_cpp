ecb72347ffd871a6439661e74da582ef12c8c913
sowish23 <soso2266@naver.com>
2021-10-19 18:37:38 +0900

    reversing array

---
+++ ReversingArray.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void reverseArray(int a[], int i, int j) {
+    if (i<j) {
+        swap(a[i], a[j]);
+        reverseArray(a, i+1, j-1);
+    }
+}
+
+int main() {
+    int a[] = {5, 7, 1, 3, 4, 9, 2, 0, 8, 6};
+    reverseArray(a, 0, 9);
+
+    for(int i=0; i<10; i++)
+        cout << a[i];
+    cout << endl;
+    return 0;
+}
